df58a1bffbfab656a3fc836fdeb35c2170913774
Administrator <Administrator@KWE-6YZS663>
2025-11-12 20:18:25 +0100

    Add project files.

---
+++ chess-proof-of-concept/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_proof_of_concept
+{
+    //TODO connect square to piece
+    //TODO should squares know their coordinates?
+    class Square
+    {
+        bool has_piece;
+        Piece piece;
+        public Square()
+        {
+            has_piece = false;
+        }
+        public Square(Piece piece)
+        {
+            this.piece = piece;
+            has_piece = true;
+        }
+        public Piece getPiece()
+        {
+            if (has_piece)
+            {
+                return piece ;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void changePiece(Piece piece)
+        {
+            has_piece = true;
+            this.piece = piece;
+        }
+    }
+    //TODO should pieces know their coordinates?
+    abstract class Piece
+    {
+        protected string symbol;
+        protected string color;
+        public string getSymbol()
+        {
+            return symbol;
+        }
+    }
+    class Pawn : Piece
+    {
+        bool first_move = true;
+        public Pawn(string color)
+        {
+            this.color = color;
+            symbol = color + "P";
+        }
+    }
+    class Chessboard
+    {
+        Square[,] board = new Square[8,8];
+        //TODO on start there should be all of the pieces present, both white and black
+        public Chessboard()
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    Square empty_square = new Square();
+                    board[i, j] = empty_square;
+                }
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                Piece p = new Pawn("b");
+                board[1, i].changePiece(p);
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                Piece p = new Pawn("w");
+                board[6, i].changePiece(p);
+            }
+
+        }
+        public void drawChessboard()
+        {
+            int n = 1;
+            int line_len = 0;
+            Console.Write("| ");
+            foreach (Square square in board)
+            {
+                string piece = "  ";
+                if (square.getPiece() != null)
+                {
+                    piece = square.getPiece().getSymbol();
+                }
+                line_len += piece.Length + 3;
+                Console.Write(piece + " | ");
+                if (n == 8)
+                {
+                    string line = "\n";
+                    for (int i = 0; i <= line_len; i++)
+                    {
+                        line = line + "-";
+                    }
+                    Console.WriteLine(line);
+                    Console.Write("| ");
+                    n = 0;
+                    line_len = 0;
+                }
+                n++;
+            }
+        }
+        Piece getPiece(int x, int y)
+        {
+            return board[x, y].getPiece();
+        }
+        public void movePiece(int x, int y, int target_x, int target_y)
+        {
+            Piece p = getPiece(x,y);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chessboard chessboard1 = new Chessboard();
+            chessboard1.drawChessboard();
+
+        }
+    }
+}
---
+++ chess-proof-of-concept/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("chess-proof-of-concept")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kingspan Environmental Ltd")]
+[assembly: AssemblyProduct("chess-proof-of-concept")]
+[assembly: AssemblyCopyright("Copyright © Kingspan Environmental Ltd 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce389298-edd6-4669-9d77-251401a7d5b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
