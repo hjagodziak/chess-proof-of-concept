2a52ea7d6606f18151440a4bfbaebbefe8501d1f
hjagodziak <hjagodziak@gmail.com>
2025-11-15 21:59:37 +0100

    First successful pawn movement using chess notation.

--- chess-proof-of-concept/Program.cs
+++ chess-proof-of-concept/Program.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace chess_proof_of_concept
 {
-    //TODO connect square to piece
-    //TODO should squares know their coordinates?
+    public struct PieceAndCoords
+    {
+        public int X { get; }
+        public int Y { get; }
+        public Piece Piece { get; }
+        public PieceAndCoords(int x, int y, Piece piece)
+        {
+            X = x;
+            Y = y;
+            this.Piece = piece;
+        }
+    }
     class Square
     {
         bool has_piece;
@@ -44,29 +54,52 @@ namespace chess_proof_of_concept
         }
     }
     //TODO should pieces know their coordinates?
-    abstract class Piece
+    public abstract class Piece
     {
-        protected string symbol;
-        protected string color;
-        public string getSymbol()
+        protected char symbol;
+        protected char color;
+        protected string displaySymbol;
+        public char getSymbol()
         {
             return symbol;
+        }
+        public char getColor()
+        {
+            return color;
+        }
+        public string getDisplaySymbol()
+        {
+            string result;
+            if (displaySymbol == null)
+            {
+                result = color.ToString() + symbol;
+                displaySymbol = result;
+                
+            }
+            else
+            {
+                result = displaySymbol;
+            }
+            return result;
         }
     }
     class Pawn : Piece
     {
         bool first_move = true;
-        public Pawn(string color)
+        public Pawn(char color)
         {
             this.color = color;
-            symbol = color + "P";
+            symbol = 'P';
+        }
+        public bool hasMoved()
+        {
+            return first_move;
         }
     }
     class Chessboard
     {
         private Square[,] board = new Square[8,8];
         //TODO on start there should be all of the pieces present, both white and black
-        //TODO the x and y coords are swapped i think
         public Chessboard()
         {
             for (int j = 0; j < 8; j++)
@@ -79,12 +112,12 @@ namespace chess_proof_of_concept
             }
             for (int i = 0; i < 8; i++)
             {
-                Piece p = new Pawn("b");
+                Piece p = new Pawn('b');
                 board[i,1].changePiece(p);
             }
             for (int i = 0; i < 8; i++)
             {
-                Piece p = new Pawn("w");
+                Piece p = new Pawn('w');
                 board[i,6].changePiece(p);
             }
 
@@ -101,7 +134,7 @@ namespace chess_proof_of_concept
                     string piece = "  ";
                     if (board[j,i].getPiece() != null)
                     {
-                        piece = board[j,i].getPiece().getSymbol();
+                        piece = board[j,i].getPiece().getDisplaySymbol();
                     }
                     line_len += piece.Length + 3;
                     Console.Write(piece + " | ");
@@ -138,15 +171,83 @@ namespace chess_proof_of_concept
         {
             return board[x, y].getPiece();
         }
+        
+        PieceAndCoords findPiece(string notation, char current_color)
+        {
+            char symbol = notation[0];
+            int column = notation[1] - 97;
+            Piece piece = null;
+            int found_row = -1;
+            //TODO add cases for other than pawn
+            switch(symbol)
+            {
+                case 'P':
+                    for (int i = 0; i < 8; i++)
+                    {
+                        if (getPiece(column, i) != null)
+                        {
+                            if (getPiece(column, i).getSymbol() == symbol && getPiece(column, i).getColor() == current_color)
+                            {
+                                piece = board[column, i].getPiece();
+                                found_row = i;
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Wrong symbol input!");break;
+            }
+
+            if (found_row != -1)
+            {
+                PieceAndCoords result = new PieceAndCoords(column, found_row, piece);
+                return result;
+            }
+            else
+            {
+                throw (new Exception("FindPiece() exception!"));
+            }
+        }
         //TODO parse chess notation (string), like Pe4 ect.
         public void movePiece(int x, int y, int target_x, int target_y)
         {
             Piece p = getPiece(x,y);
-            //if (getPiece(target_x,target_y) == null)
+            if (getPiece(target_x,target_y) == null)
             {
                 board[x, y].removePiece();
                 board[target_x, target_y].changePiece(p);
             }
+        }
+        public void movePiece(string notation, char current_color)
+        {
+            if (notation.Length == 3)
+            {
+                char symbol = notation[0];
+                int column = notation[1] - 97;
+                int row = notation[2] - 48;
+
+                PieceAndCoords piece = findPiece(notation, current_color);
+
+                //moving pawn
+                if (piece.Piece != null)
+                {
+                    movePiece(piece.X,piece.Y,column,row);
+                }
+                else
+                {
+                    Console.WriteLine("Piece not found!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong notation syntax!");
+            }
+
+            /*Console.WriteLine(symbol);
+            Console.WriteLine(row);
+            Console.WriteLine(column);
+            Console.WriteLine();*/
         }
     }
     internal class Program
@@ -154,10 +255,16 @@ namespace chess_proof_of_concept
         static void Main(string[] args)
         {
             Chessboard chessboard1 = new Chessboard();
-            
+            string read = "";
             chessboard1.drawChessboard();
-            chessboard1.movePiece(6, 2, 4, 2);
-            chessboard1.drawChessboard();
+            while (read != "end")
+            {
+                Console.WriteLine("Podaj komendę: ");
+                read = Console.ReadLine();
+                Console.WriteLine();
+                chessboard1.movePiece(read, 'w');
+                chessboard1.drawChessboard(); 
+            }
         }
     }
 }
